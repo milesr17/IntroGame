191db86b42bb21ac4d33700545e342a8a3bcee10
Robel-Tadele <arttesema@gmail.com>
2021-10-08 10:58:43 +0100

    lab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
+    private int count;
     public Vector2 moveValue;
     public float speed;
+    int count=0;
 
     void OnMove(InputValue value)
     {
@@ -19,4 +22,20 @@ public class PlayerController : MonoBehaviour
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
     }
+    
+    void OnTriggerEnter ( Collider other ) {
+        if( (other . gameObject . tag == " PickUp " ) && (count++)) {
+            other . gameObject . SetActive ( false );
+}
+    }
+    // void Start()
+    // {
+    //     int count=0;
+    //     if(other . gameObject . tag == "PickUp"){
+    //         count++;
+    //     }
+
+    // }
+
+
 }
